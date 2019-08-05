bbc20da2c8db28da38f1ec184ddbe3cf067a31c5
Denis Golubev <maxmadnes@mail.ru>
2019-08-05 19:32:41 +0300

    fix js script

--- js/modals.js
+++ js/modals.js
@@ -1,8 +1,7 @@
 'use strict';
 
 (function () {
-
-
+    
     let linkMap = document.querySelector('.js-map');
     let linksBuy = document.querySelectorAll('.js-buy');
     let linkWrite = document.querySelector('.js-write-us');
@@ -12,46 +11,49 @@
     let closeMap = document.querySelector('.modal-close-map');
     let closeBuy = document.querySelector('.close-modal-buy');
     let closeWrite = document.querySelector('.modal-write-close');
-    
-    const openModal = function (modalName, classToAdd) {
-        if (!modalName.classList.contains(classToAdd)) {
-            modalName.classList.add(classToAdd);
+
+    const openModal = function (modal, addClass) {
+        if (!modal.classList.contains(addClass)) {
+            modal.classList.add(addClass);
         }
         window.addEventListener('keydown', function (e) {
-            if (e.keyCode === 27 && modalName.classList.contains(classToAdd)) {
-                closeModal(modalName, classToAdd);
+            if (e.keyCode === 27 && modal.classList.contains(addClass)) {
+                closeModal(modal, addClass);
             }
         });
     };
 
 
-    const closeModal = function (modalName, classToDel) {
-        if (modalName.classList.contains(classToDel)) {
-            modalName.classList.remove(classToDel);
+    const closeModal = function (modal, removeClass) {
+        if (modal.classList.contains(removeClass)) {
+            modal.classList.remove(removeClass);
         }
     };
 
+    if (mapWindow) {
+        linkMap.addEventListener('click', function (e) {
+            e.preventDefault();
+            openModal(mapWindow, 'show-modal-map');
+        });
 
-    linkMap.addEventListener('click', function (e) {
-        e.preventDefault();
-        openModal(mapWindow, 'show-modal-map');
-    });
+        closeMap.addEventListener('click', function (e) {
+            e.preventDefault();
+            closeModal(mapWindow, 'show-modal-map');
+        });
 
-    closeMap.addEventListener('click', function (e) {
-        e.preventDefault();
-        closeModal(mapWindow, 'show-modal-map');
-    });
+    }
 
+    if (writeWindow) {
+        linkWrite.addEventListener('click', function (e) {
+            e.preventDefault();
+            openModal(writeWindow, 'show-modal-content');
+        });
 
-    linkWrite.addEventListener('click', function (e) {
-        e.preventDefault();
-        openModal(writeWindow, 'show-modal-content');
-    });
-
-    closeWrite.addEventListener('click', function (e) {
-        e.preventDefault();
-        closeModal(writeWindow, 'show-modal-content');
-    });
+        closeWrite.addEventListener('click', function (e) {
+            e.preventDefault();
+            closeModal(writeWindow, 'show-modal-content');
+        });
+    }
 
     for (let i = 0; i < linksBuy.length; i++) {
         linksBuy[i].addEventListener('click', function (e) {
